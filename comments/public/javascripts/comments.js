eb938239bce293b3ff8a8060100e26a1b2735efa
Lucien <lucien.briscoe@gmail.com>
2016-03-11 06:37:24 +0000

    Added Styling to make it look nice

--- comments/public/javascripts/comments.js
+++ comments/public/javascripts/comments.js
@@ -1,31 +1,33 @@
 $(document).ready(function(){
     $("#serialize").click(function(){
-        var myobj = {Name:$("#Name").val(),Comment:$("#Comment").val()};
-        jobj = JSON.stringify(myobj);
-        $("#json").text(jobj);
+      event.preventDefault();
+      var myobj = {Name:$("#Name").val(),Comment:$("#Comment").val()};
+      jobj = JSON.stringify(myobj);
+      $("#json").html("JSON Object Created: " + jobj);
 
-        var url = "comment";
-		$.ajax({
-		  url:url,
-		  type: "POST",
-		  data: jobj,
-		  contentType: "application/json; charset=utf-8",
-		  success: function(data,textStatus) {
-		      $("#done").html(textStatus);
-		  }
-		});
+      var url = "comment";
+
+  		$.ajax({
+  		  url:url,
+  		  type: "POST",
+  		  data: jobj,
+  		  contentType: "application/json; charset=utf-8",
+  		  success: function(data,textStatus) {
+  		      $("#done").html("Status: " + textStatus);
+  		  }
+  		});
 
     });
 
     $("#getThem").click(function() {
       $.getJSON('comment', function(data) {
         console.log(data);
-        var everything = "<ul>";
+        var everything = "<div class='list-group'>";
         for(var comment in data) {
           com = data[comment];
-          everything += "<li>Name: " + com.Name + " -- Comment: " + com.Comment + "</li>";
+          everything += "<a href='#' class='list-group-item'><p class='list-group-item-text'>\"" + com.Name + "\" Said:</p><h4 class='list-group-item-heading'>\"" + com.Comment + "\"</h4></a>";
         }
-        everything += "</ul>";
+        everything += "</div>";
         $("#comments").html(everything);
       })
     })
